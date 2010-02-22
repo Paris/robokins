ba9ea398763c111150ee97470f3fe340df1bcf8a
polyethene <poly@ethene.net>
2010-02-22 09:58:09 +0000

    Changed another class to use getter methods.

--- IRC/User.cs
+++ IRC/User.cs
@@ -4,37 +4,52 @@ namespace robokins.IRC
 {
     class User
     {
-        public string Nick;
-        public string Ident;
-        public string Host;
+        string nick;
+        string ident;
+        string host;
 
         public User(string alias)
         {
             int x = alias.IndexOf('!');
-            Nick = x == -1 ? string.Empty : alias.Substring(0, x);
+            nick = x == -1 ? string.Empty : alias.Substring(0, x);
 
             x++;
             int y = alias.IndexOf('@', x);
-            Ident = y == -1 ? string.Empty : alias.Substring(x, y - x);
+            ident = y == -1 ? string.Empty : alias.Substring(x, y - x);
 
             y++;
-            Host = alias.Substring(y);
+            host = alias.Substring(y);
+        }
+
+        public string Nick
+        {
+            get { return nick; }
+        }
+
+        public string Ident
+        {
+            get { return ident; }
+        }
+
+        public string Host
+        {
+            get { return host; }
         }
 
         public override string ToString()
         {
-            var buf = new StringBuilder(Nick.Length + Ident.Length + Host.Length + 2);
+            var buf = new StringBuilder(Nick.Length + ident.Length + host.Length + 2);
             if (Nick.Length != 0)
             {
                 buf.Append(Nick);
                 buf.Append('!');
             }
-            if (Ident.Length != 0)
+            if (ident.Length != 0)
             {
-                buf.Append(Ident);
+                buf.Append(ident);
                 buf.Append('@');
             }
-            buf.Append(Host);
+            buf.Append(host);
             return buf.ToString();
         }
     }
