e56c4d6b1620d68dbe36168861eea5eedb30cfa7
polyethene <poly@ethene.net>
2011-01-31 14:05:02 +0000

    Fixed Urban Dictionary search.

--- Utility/Search/UrbanDictionary.cs
+++ Utility/Search/UrbanDictionary.cs
@@ -6,20 +6,10 @@ namespace robokins.Utility.Search
     {
         public static string Search(string term)
         {
-            string html = HTTP.DownloadPage("http://www.urbandictionary.com/define.php?term=" + HttpUtility.UrlEncode(term));
-
-            const string boundary = "<div class='definition'>";
-            int pos = html.IndexOf(boundary);
-            if (pos == -1)
-                return null;
-
-            pos += boundary.Length;
-            html = html.Substring(pos, html.IndexOf("</div>", pos) - pos).Replace("<br>", "\n").Trim();
+            var html = HTTP.DownloadPage("http://www.urbandictionary.com/define.php?term=" + HttpUtility.UrlEncode(term));
+            var z = html.IndexOf("<table id='entries'>");
+            html = Texts.StringBetween(html, "<div class=\"definition\">", "</div>", z);
             html = Texts.StripTags.Replace(html, string.Empty);
-            pos = html.IndexOf("\n");
-            if (pos != -1)
-                html = html.Substring(0, pos);
-
             return HttpUtility.HtmlDecode(html);
         }
     }
