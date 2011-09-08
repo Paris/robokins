1c2f9e8935649e4907a76942c5abce41ecb1a7bc
polyethene <poly@ethene.net>
2011-09-08 20:58:10 +0100

    Check for null references.

--- Utility/Search/UrbanDictionary.cs
+++ Utility/Search/UrbanDictionary.cs
@@ -8,7 +8,11 @@ namespace robokins.Utility.Search
         {
             var html = HTTP.DownloadPage("http://www.urbandictionary.com/define.php?term=" + HttpUtility.UrlEncode(term));
             var z = html.IndexOf("<table id='entries'>");
+            if (z == -1)
+                return null;
             html = Texts.StringBetween(html, "<div class=\"definition\">", "</div>", z);
+            if (string.IsNullOrEmpty(html))
+                return null;
             html = Texts.StripTags.Replace(html, string.Empty);
             return HttpUtility.HtmlDecode(html);
         }
