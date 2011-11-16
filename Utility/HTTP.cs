fbb5d0cc034a462f5b5b396d1a1d9a1beb804d19
polyethene <poly@ethene.net>
2011-11-16 16:28:10 +0000

    Changed download methods.

--- Utility/HTTP.cs
+++ Utility/HTTP.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Reflection;
 using System.Text;
+using System.Web;
 
 namespace robokins.Utility
 {
@@ -38,26 +40,43 @@ namespace robokins.Utility
             }
         }
 
-        public static string DownloadPage(string uri, string data, NetworkCredential auth = null, string contentType = null)
+        public static HttpWebResponse RequestPage(string uri, IDictionary<string, string> parameters, string method = "POST")
+        {
+            var data = new StringBuilder();
+
+            foreach (var key in parameters.Keys)
+            {
+                data.Append(key);
+                data.Append('=');
+                data.Append(HttpUtility.UrlEncode(parameters[key]));
+                data.Append('&');
+            }
+
+            data.Remove(data.Length - 1, 1);
+
+            ServicePointManager.Expect100Continue = false;
+            byte[] buffer = Encoding.ASCII.GetBytes(data.ToString());
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
+
+            req.Method = method;
+
+            if (req.Method.Equals("POST"))
+                req.ContentType = "application/x-www-form-urlencoded";
+
+            req.ContentLength = buffer.Length;
+
+            Stream post = req.GetRequestStream();
+            post.Write(buffer, 0, buffer.Length);
+            post.Close();
+
+            return (HttpWebResponse)req.GetResponse();
+        }
+
+        public static string DownloadPage(string uri, IDictionary<string, string> parameters, string method = "POST")
         {
             try
             {
-                ServicePointManager.Expect100Continue = false;
-                byte[] buffer = Encoding.ASCII.GetBytes(data);
-                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri);
-
-                if (auth != null)
-                    req.Credentials = auth;
-
-                req.Method = "POST";
-                req.ContentType = contentType ?? "application/x-www-form-urlencoded";
-                req.ContentLength = buffer.Length;
-
-                Stream post = req.GetRequestStream();
-                post.Write(buffer, 0, buffer.Length);
-                post.Close();
-
-                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                var resp = RequestPage(uri, parameters, method);
                 return (new StreamReader(resp.GetResponseStream())).ReadToEnd();
             }
             catch (Exception)
