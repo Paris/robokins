67e7e241a7e5858378cbe3d06763f33ce5cfb209
polyethene <poly@ethene.net>
2011-11-16 21:20:52 +0000

    Localise constant.

--- Utility/Search/MSDN.cs
+++ Utility/Search/MSDN.cs
@@ -5,11 +5,10 @@ namespace robokins.Utility.Search
 {
     class MSDN
     {
-        const string URL = "http://social.msdn.microsoft.com/search/en-US/feed?refinement=181&format=RSS&query=";
-
         public static string[] Search(string query)
         {
-            string xml = HTTP.DownloadPage(URL + HttpUtility.UrlEncode(query));
+            const string url = "http://social.msdn.microsoft.com/search/en-US/feed?refinement=181&format=RSS&query=";
+            string xml = HTTP.DownloadPage(url + HttpUtility.UrlEncode(query));
 
             GroupCollection group = Texts.ItemDescrRSS.Match(xml).Groups;
             if (group.Count < 4 || group[2].Value == string.Empty)
